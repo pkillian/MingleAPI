377f39b68fdbeb1f4276e88e4f96f750dd7a99c2
Aaron Salvo <aaron.salvo@teamaol.com>
2013-12-03 14:24:47 -0500

    Stubs and SQL added

--- MingleAPI/__init__.py
+++ MingleAPI/__init__.py
@@ -1,13 +1,29 @@
+from sqlalchemy import *
+from sqlalchemy.engine.url import URL
+from contextlib import closing
 from flask import Flask
 app = Flask(__name__)
 
 import json
 import socket
 
+import MingleAPI.settings
 import MingleAPI.authentication
 import MingleAPI.messaging
 
 @app.route("/")
 def health_check():
     return socket.gethostname();
+
+def connect_db():
+    return create_engine(URL(**settings.DATABASE))
+
+def init_db():
+    db = connect_db();
+    metadata = MetaData(db)
+    tbl = Table("user_profiles", metadata,
+            Column('username', String),
+            Column('gender', String(1)),
+            Column('topics', Integer))
+    tbl.create()
 
---
+++ MingleAPI/settings.py
@@ -0,0 +1,5 @@
+DATABASE = {'drivername' : 'postgresql',
+            'host' : 'hackathon-mtc02.aol.com',
+            'port' : '5432',
+            'username' : 'mingle_db',
+            'database' : 'mingle_db'}
